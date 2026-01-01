ac589b70f5d18ec813ba7fa9d53536ebf72b94c6
Alexander Yu <156768800+L-A-G-B-O-T@users.noreply.github.com>
2026-01-01 01:04:44 +0000

    Set up testing apparatus for coded bias

--- CNN/NeuralNet_Images.py
+++ CNN/NeuralNet_Images.py
@@ -1,36 +1,91 @@
-import pandas as pd
 import numpy as np
+from random import shuffle
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
-from keras.losses import SparseCategoricalCrossentropy
-from keras.metrics import Accuracy, SparseCategoricalAccuracy
-from matplotlib import pyplot as plt
+from keras.losses import BinaryCrossentropy
+from keras.metrics import BinaryAccuracy
 from custom_models import Regular, FSRS
 import pickle
 
-with open("MNIST/All.pkl", "rb") as outfile:
-    train_images, train_labels = pickle.load(outfile)
+loaded_train_images : list[list] = [None]*10 # pyright: ignore[reportAssignmentType]
 
-print("Frequencies: ")
+for i in range(10):
+    with open("MNIST/All"+str(i)+"s.pkl", "rb") as infile:
+        loaded_train_images[i] = pickle.load(infile)
 
-train_images = train_images / 255 #normalize color values to floats between 0 and 1
+def load_train_images_to_Array(arr:list, targetLabel:int, maxSize:int=99999999999) -> None:
+    with open("MNIST/All"+str(targetLabel)+"s.pkl", "rb") as infile:
+        train_images = pickle.load(infile)
 
-model = FSRS([
-    Flatten(input_shape=(28,28)),
-    Dense(16, activation="relu"),
-    Dense(10),
-])
+    for i in range(len(train_images)):
+        if len(arr) >= maxSize:
+            return
+        arr.append(train_images[i])
 
-#compile model
-model.compile(optimizer='adam',
-              loss=SparseCategoricalCrossentropy(from_logits=True), #don't worry too much about logits
-              metrics=[SparseCategoricalAccuracy()])
+def test(majorityDigit: int, minorityDigit: int, majorityDigitCount:int = 5421, minorityDigitCount:int=2711, fillerCount:int=8132):
+    assert majorityDigitCount >= minorityDigitCount
+    train_images_majority = []
+    train_images_minority = []
+    train_images_filler = []
 
-model.fit(train_images, train_labels, epochs=2)
+    load_train_images_to_Array(train_images_majority, targetLabel=majorityDigit, maxSize=majorityDigitCount)
+    load_train_images_to_Array(train_images_minority, targetLabel=minorityDigit, maxSize=minorityDigitCount)
+    for i in range(10):
+        if i == majorityDigit or i == minorityDigit:
+            continue
+        load_train_images_to_Array(train_images_filler, targetLabel=i)
+    
+    newIndexes = list(range(len(train_images_filler)))
+    shuffle(newIndexes)
+    train_images_filler_shuffled = [train_images_filler[newIndexes[i]] for i in range(fillerCount)]
+    train_images_array = train_images_majority + train_images_minority + train_images_filler_shuffled
+    train_labels_array = [1]*(majorityDigitCount+minorityDigitCount) + [0]*fillerCount
+    newIndexes = list(range(majorityDigitCount+minorityDigitCount+fillerCount))
+    shuffle(newIndexes)
+    train_images = np.array([train_images_array[newIndex] for newIndex in newIndexes])
+    train_labels = np.array([train_labels_array[newIndex] for newIndex in newIndexes])
 
-with open("MNIST/Test.pkl", "rb") as outfile:
-    test_images, test_labels = pickle.load(outfile)
+    train_images = train_images / 255 #normalize color values to floats between 0 and 1
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2) # type: ignore
+    model = Regular([
+        Flatten(input_shape=(28,28)),
+        Dense(4, activation="relu"),
+        Dense(1, activation="sigmoid"),
+    ])
 
-print('\nTest accuracy:', test_acc)
+    #compile model
+    model.compile(optimizer='adam',
+                loss=BinaryCrossentropy(), 
+                metrics=[BinaryAccuracy()])
+
+    model.fit(train_images, train_labels, epochs=2)
+
+    with open("MNIST/Test"+str(majorityDigit)+"s.pkl", "rb") as infile:
+        test_images_majority = pickle.load(infile)
+
+    test_labels_majority = [1] * len(test_images_majority)
+
+    test_loss, test_acc = model.evaluate(np.array(test_images_majority),  np.array(test_labels_majority), verbose=0) # type: ignore
+
+    print(f'\nMajority digit {majorityDigit}: {round(test_acc*len(test_labels_majority))} correct, {round((1-test_acc)*len(test_labels_majority))} incorrect')
+
+    with open("MNIST/Test"+str(minorityDigit)+"s.pkl", "rb") as infile:
+        test_images_minority = pickle.load(infile)
+
+    test_labels_minority = [1] * len(test_images_minority)
+
+    test_loss, test_acc = model.evaluate(np.array(test_images_minority),  np.array(test_labels_minority), verbose=0) # type: ignore
+
+    print(f'\nMinority digit {minorityDigit}: {round(test_acc*len(test_labels_minority))} correct, {round((1-test_acc)*len(test_labels_minority))} incorrect')
+
+    return {"majority" : [round((1-test_acc)*len(test_labels_majority)), round(test_acc*len(test_labels_majority))],
+            "minority" : [round((1-test_acc)*len(test_labels_minority)), round(test_acc*len(test_labels_minority))]}
+
+test(majorityDigit=0,minorityDigit=4)
+exit()
+
+for i in range(0,10):
+    for j in range(0, 10):
+        if i == j:
+            continue
+        log = test(majorityDigit=i,minorityDigit=j)
